c8c9f59e445cf974a51106f1250ddcd6af82938a
ckgathi <ckgathi@users.noreply.github.com>
2020-09-22 16:22:50 +0200

    re arange navbars

--- flourish/navbars.py
+++ flourish/navbars.py
@@ -6,17 +6,17 @@ flourish = Navbar(name='flourish')
 
 flourish.append_item(
     NavbarItem(
-        name='maternal_screening',
-        label='Maternal Screening',
-        fa_icon='far fa-user-circle',
-        url_name=settings.DASHBOARD_URL_NAMES.get('maternal_screening_listboard_url')))
-
-flourish.append_item(
-    NavbarItem(
         name='maternal_locator',
         label='Maternal Locator',
         fa_icon='far fa-user-circle',
         url_name=settings.DASHBOARD_URL_NAMES.get('maternal_locator_listboard_url')))
+
+flourish.append_item(
+    NavbarItem(
+        name='maternal_screening',
+        label='Maternal Screening',
+        fa_icon='far fa-user-circle',
+        url_name=settings.DASHBOARD_URL_NAMES.get('maternal_screening_listboard_url')))
 
 flourish.append_item(
     NavbarItem(

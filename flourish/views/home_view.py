0d5867695ec54f6d78a3887d6498b7f9e7adf0bf
COulson Kgathi <ckgathi@gmail.com>
2020-12-07 12:48:11 +0200

    update home report

--- flourish/views/home_view.py
+++ flourish/views/home_view.py
@@ -2,9 +2,24 @@ from django.views.generic import TemplateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_navbar import NavbarViewMixin
 
+from flourish_caregiver.models import SubjectConsent
+
 
 class HomeView(EdcBaseViewMixin, NavbarViewMixin, TemplateView):
 
     template_name = 'flourish/home.html'
     navbar_name = 'flourish'
     navbar_selected_item = 'home'
+
+    @property
+    def total_flourish_consents(self):
+        """Returns flourish consents.
+        """
+        return SubjectConsent.objects.all().count()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update(
+            flourish_consents=self.total_flourish_consents)
+        return context
